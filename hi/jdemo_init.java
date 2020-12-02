80fd44efd685a155cd6edaa5d4bcd1209c135a78
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-12-02 09:11:52 +0800

    update

--- hi/jdemo_init.java
+++ hi/jdemo_init.java
@@ -11,6 +11,7 @@ import java.util.regex.Matcher;
 import com.samskivert.mustache.Template;
 import com.samskivert.mustache.Mustache;
 import com.google.gson.Gson;
+import org.apache.commons.codec.digest.DigestUtils;
 
 class jdemo_init {
     private static hi.route r = hi.route.get_instance();
@@ -139,17 +140,7 @@ class jdemo_init {
     }
 
     private String md5(String str) {
-        try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(str.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
-            }
-            return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
-        }
-        return null;
+        return DigestUtils.md5Hex(str);
     }
 
     private String do_foreach(HashMap<String, String> m) {
