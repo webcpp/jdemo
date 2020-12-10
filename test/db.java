892e7cdb132aae980a55c1db0aa0c9d34fc5d8bc
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-12-10 08:59:02 +0800

    update

--- test/db.java
+++ test/db.java
@@ -3,16 +3,21 @@ package test;
 import hi.request;
 import hi.response;
 import hi.route;
+
 import java.util.regex.Matcher;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
+import java.util.Map;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 
 import org.mariadb.jdbc.MariaDbPoolDataSource;
 
@@ -48,26 +53,13 @@ public class db implements hi.route.run_t {
 
     public void handler(hi.request req, hi.response res, Matcher m) {
         String sql = "SELECT * FROM `article` ORDER BY `id` LIMIT 0,5;";
-        ResultSetHandler<ArrayList<HashMap<String, Object>>> h = (ResultSet rs) -> {
-            ArrayList<HashMap<String, Object>> result = new ArrayList<HashMap<String, Object>>();
-            while (rs.next()) {
-                HashMap<String, Object> row = new HashMap<String, Object>();
-                ResultSetMetaData meta = rs.getMetaData();
-                int cols = meta.getColumnCount();
-
-                for (int i = 0; i < cols; i++) {
-                    row.put(meta.getColumnName(i + 1), rs.getObject(i + 1));
-                }
-                result.add(row);
-            }
-            return result;
-        };
         try {
-            QueryRunner qr = new QueryRunner(db_help.get_instance().get_data_source());
-            ArrayList<HashMap<String, Object>> result = qr.query(sql, h);
+            Connection conn = db_help.get_instance().get_data_source().getConnection();
+            QueryRunner qr = new QueryRunner();
+            List<Map<String, Object>> result = qr.query(conn, sql, new MapListHandler());
             StringBuffer content = new StringBuffer();
-            for (HashMap<String, Object> item : result) {
-                for (HashMap.Entry<String, Object> iter : item.entrySet()) {
+            for (Map<String, Object> item : result) {
+                for (Map.Entry<String, Object> iter : item.entrySet()) {
                     content.append(String.format("%s = %s\n", iter.getKey(), iter.getValue().toString()));
                 }
                 content.append("\n\n");
@@ -75,6 +67,7 @@ public class db implements hi.route.run_t {
 
             res.content = content.toString();
             res.status = 200;
+            DbUtils.close(conn);
         } catch (SQLException e) {
             res.content = e.getMessage();
             res.status = 500;
