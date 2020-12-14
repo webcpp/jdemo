d03cae850fbaa5ee155eb533c2d7de5270c5a0b8
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-12-14 22:21:53 +0800

    update

--- website/db_help.java
+++ website/db_help.java
@@ -1,5 +1,6 @@
 package website;
 
+import java.lang.reflect.Method;
 import java.sql.SQLException;
 import com.alibaba.druid.pool.DruidDataSource;
 
@@ -24,6 +25,16 @@ public class db_help {
         db_help.ds.setUrl(hi.route.get_instance().get_config().getString("mariadb.url"));
         db_help.ds.setUsername(hi.route.get_instance().get_config().getString("mariadb.username"));
         db_help.ds.setPassword(hi.route.get_instance().get_config().getString("mariadb.password"));
+        
+        db_help.ds.setInitialSize(hi.route.get_instance().get_config().getInt("druid.initialSize"));
+        db_help.ds.setMaxActive(hi.route.get_instance().get_config().getInt("druid.maxActive"));
+        db_help.ds.setMaxWait(hi.route.get_instance().get_config().getLong("druid.maxWait"));
+        db_help.ds.setMinIdle(hi.route.get_instance().get_config().getInt("druid.minIdle"));
+        db_help.ds.setValidationQuery(hi.route.get_instance().get_config().getString("druid.validationQuery"));
+        db_help.ds.setTestWhileIdle(hi.route.get_instance().get_config().getBoolean("druid.testWhileIdle"));
+        db_help.ds.setTestOnBorrow(hi.route.get_instance().get_config().getBoolean("druid.testOnBorrow"));
+        db_help.ds.setTestOnReturn(hi.route.get_instance().get_config().getBoolean("druid.testOnReturn"));
+
         return db_help.ds;
     }
 }
