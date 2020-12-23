6276f954dea6058ea4ba9670a118b256a37791b1
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-12-23 14:57:16 +0800

    update

--- mykotlin/test.kt
+++ mykotlin/test.kt
@@ -5,9 +5,8 @@ import hi.response
 import hi.route
 import java.util.regex.Matcher
 
-
-class test:hi.route.run_t {
-    override fun handler(req: hi.request, res: hi.response, m: Matcher){
+class test : hi.route.run_t {
+    override fun handler(req: hi.request, res: hi.response, m: Matcher) {
         res.content = "hello,kotlin\n"
         res.status = 200
         res.set_content_type("text/plain;charset=utf-8")
